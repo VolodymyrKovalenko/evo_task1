b3023a33b0a3ea2f94e59766e25cab2d23eb9ae8
volodymyr <whitelol888@gmail.com>
2018-06-06 18:10:15 +0300

    commit

--- SQLAlchemy_db.py
+++ SQLAlchemy_db.py
@@ -5,7 +5,7 @@ from flask_script import Manager
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get['postgresql-vertical-91489']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('postgresql-vertical-91489')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
